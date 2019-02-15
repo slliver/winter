683887cc7d41d0e71f816aa4a065e8ba5f6faf0a
zhaozhengchao <zhaozhengchao>
2019-02-15 09:34:07 +0800

    ##

--- src/main/java/com/slliver/dao/ApiLoanDataMapper.java
+++ src/main/java/com/slliver/dao/ApiLoanDataMapper.java
@@ -11,7 +11,17 @@ public interface ApiLoanDataMapper extends RobinMapper<ApiLoanData> {
 
     List<ApiLoanData> selectListByPage(BaseSearchCondition condition);
 
+    /**
+     * (旧版本，适合App)极速贷接口
+     * @param condition
+     * @return
+     */
     List<ApiLoanData> selectListByApi(BaseSearchCondition condition);
 
+    /**
+     * (新版本，适合H5)极速贷接口
+     * @param condition
+     * @return
+     */
     List<ApiLoanData> selectListByApiNoPagination(BaseSearchConditionWithoutPagination condition);
 }
